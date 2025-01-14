7efd2ebc05a3abb124052862b775f18a362437d2
sahil8443 <shejalsahil3@gmail.com>
2025-01-14 21:26:22 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,16 @@
+int reverse(int x) {
+    long sum = 0;
+
+    while (x != 0) {
+        int rem = x % 10;
+        sum = sum * 10 + rem;
+        x = x / 10;
+    }
+
+    // Check for overflow (32-bit signed integer range)
+    if (sum > INT_MAX || sum < INT_MIN) {
+        return 0;
+    }
+
+    return (int)sum;
+}
